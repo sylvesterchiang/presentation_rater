cd8e6ff5b26dfa4ab5b9014e813ffd693c116e03
Sylvester Chiang <sylvester_ch7593@hotmail.com>
2016-10-22 09:42:19 -0400

    collecting emotion information

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var emotions = require('./routes/emotions');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.post('/emotions/:datauri', emotions);
+app.get('/emotions/:something', emotions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/emotions.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var config = require('config');
+
+var apiUrl = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+
+var toBlob = function(data_uri, type) {
+    var bytes = atob(data_uri.split(',')[1])
+    var arr = new Uint8Array(bytes.length);
+    for (var i = 0; i < bytes.length; i++) {
+    	arr[i] = bytes.charCodeAt(i);
+    }
+  return new Blob([arr], { type: type || 'image/png' });
+};
+
+/* GET home page. */
+router.post('/emotions/:datauri', function(req, res, next) {
+
+	console.log('what the fruit');
+
+	console.log(req.params.datauri.replace(/-/g, '\/'));
+	temp = toBlob(req.params.datauri.replace(/-/g, '\/'), "image/png");
+	/*$.ajax({
+		url: apiUrl, 
+		beforeSend: function (xhrObj){
+			xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+			xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", config.get('emotion_key'));
+		},
+		type: "POST", 
+		data: toBlob(req.params[datauri].replace(/\-/g, '\/'), "image/png"), 
+		processData: false
+	})
+	.done(function(response){
+		res.send(response);
+	});*/
+});
+
+router.get('/emotions/:something', function(req, res, next){
+	res.send(req.params.something)
+});
+
+module.exports = router;
